9952f9133eab55d41d142cebec82e716626530f9
victor <victor>
2014-05-19 17:56:17 +0530

    initial codebase commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterApp::Application.config.secret_token = 'f8835446a9846380491be35c1cef4c2bc273eb2d4b1758940e128a0df66ae26f41795462b4aab0c7b6028efecb13fcf7ccc58951ea16fbc7813ec1f65d1985b9'
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,7 @@
+TweetStream.configure do |config|
+  config.consumer_key       = 'vXqmJQmD1evGE9THVIXxJncFF'
+  config.consumer_secret    = 'mJG75Mv4mg07YWIJKL3cF9TRo6UKUwz2g1FGqrvjnjsoeXosvE'
+  config.oauth_token        = '2504560932-JLTXuZFizzm4IXAsqEtYQ857WIJeCcxwvmYYc9H'
+  config.oauth_token_secret = '0kUe6Tkfegi0LcOBbW7UnHwJ5nQvo58Rk0sTHNw2tpDwe'
+  config.auth_method        = :oauth
+end
